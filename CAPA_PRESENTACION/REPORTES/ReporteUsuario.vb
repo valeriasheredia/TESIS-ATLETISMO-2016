67f0817b7ca12e8b0627f0b35829774193a5356c
valeriasheredia <valeheredia97@gmail.com>
2020-09-16 14:09:34 -0300

    Add files via upload

---
+++ CAPA_PRESENTACION/REPORTES/ReporteUsuario.vb
@@ -0,0 +1,13 @@
+﻿Imports CrystalDecisions.Shared
+
+Public Class ReporteUsuario
+
+    Private Sub ReporteUsuario_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        Dim objreporte As New ReporteUsuario
+
+        CrystalReportViewer1.ReportSource = objreporte
+
+    End Sub
+End Class
+
